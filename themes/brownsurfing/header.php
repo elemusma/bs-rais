51448524598e094f35db57e0ab8bf86683349827
Efrain Lemus <efraintlemusm@gmail.com>
2022-12-05 19:27:12 -0500

    update button message

--- themes/brownsurfing/header.php
+++ themes/brownsurfing/header.php
@@ -142,7 +142,7 @@ $logo = get_field('logo','options');
     echo '<h1 class="text-white bold" style="">All-in-one Strategy and Technology Solution</h1>';
     echo '<h6 class="text-accent-tertiary text-uppercase">where innovation accelerates impact</h6>';
     echo '<p class="text-white">We partner with leading social enterprises, nonprofits, corporations, and purpose-driven leaders to deliver solutions that tackle today\'s urgent challenges. RAIS3 is a team of strategists who match ingenuity with technology to unlock new funding, deepen engagement, and generate sustainable and scalable impact for the causes and communities our clients champion. From strategy to planning, implementation to execution, we bring expertise to realize exceptional value and transformative impact.</p>';
-    echo '<span class="btn bg-white text-accent contact-us open-modal" id="contact-us" style="">Send Us a Message</span>';
+    echo '<span class="btn bg-white text-accent contact-us open-modal" id="contact-us" style="">Contact Us</span>';
 echo '</div>';
 
     // echo '<div class="col-lg-6 col-md-9">';
