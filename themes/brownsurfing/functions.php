7374ddad8c74f8a1a523c6015f83a9f738fa1b8a
Efrain Lemus <efraintlemusm@gmail.com>
2022-12-13 20:35:14 -0500

    style contact form

--- themes/brownsurfing/functions.php
+++ themes/brownsurfing/functions.php
@@ -192,3 +192,20 @@ function btn_shortcode( $atts, $content = null ) {
 	}
 	
 	add_shortcode( 'button', 'btn_shortcode' );
+
+function spacer_shortcode( $atts, $content = null ) {
+
+	$a = shortcode_atts( array(
+
+		'class' => '',
+
+		'style' => ''
+
+	), $atts );
+
+	return '<div class="spacer ' . esc_attr($a['class']) . '" style="' . esc_attr($a['style']) . '"></div>';
+
+	// [spacer class="" style=""]
+}
+
+add_shortcode( 'spacer', 'spacer_shortcode' );
